40775e798cbbe730601256db02ad40debb44c6da
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-06-09 03:06:03 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replece):
+def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in range(len(new_list)):
         if new_list[i] == search:
